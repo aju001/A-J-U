48c55b4a4038a79a1b8d25d4100db552ee3e85eb
Aju <94348613+aju001@users.noreply.github.com>
2021-11-24 21:20:22 +0530

    A-J-U

---
+++ plugins/stopjadibot.js
@@ -0,0 +1,22 @@
+let handler  = async (m, { conn }) => {
+    if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Why dont you go directly to the terminal??', m)
+    else {
+      await conn.reply(m.chat, 'Goodbye bot :\')', m)
+      conn.close()
+    }
+  }
+  handler.help = ['stop']
+  handler.tags = ['jadibot']
+  handler.command = /^(mathi|stop)$/i
+  handler.owner = true
+  handler.mods = false
+  handler.premium = false
+  handler.group = false
+  handler.private = false
+  
+  handler.admin = false
+  handler.botAdmin = false
+  
+  handler.fail = null
+  
+  module.exports = handler
