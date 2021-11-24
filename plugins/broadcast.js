3985e78efeb92bc70b8728119ca57dc977bd4f19
Aju <94348613+aju001@users.noreply.github.com>
2021-11-25 01:58:56 +0530

    A-J-U

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chat_\nEstimation complete ${chats.length * 1.5} second`, m)
   for (let id of chats) {
     await delay(1500)
-    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Eva Broadcast 〕\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 𝘼𝙟𝙪 𝘽𝙧𝙤𝙖𝙙𝙘𝙖𝙨𝙩 〕\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
   }
   m.reply('_*Broadcast Complete*_')
 }
