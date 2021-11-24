c4039e99b3bbaf328ad7c3e814fabfb20576f2c1
Aju <94348613+aju001@users.noreply.github.com>
2021-11-24 20:34:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,14 +31,14 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'Eva-Sticker'
-global.author = 'Eva'
+global.packname = 'Aju-Sticker'
+global.author = 'Aju'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://ibb.co/jW31z7n'
 
-global.watermark = '© EVA'
+global.watermark = '© AJU'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
