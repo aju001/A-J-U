fb933c1630cf5015b17d43f36f2d5a85b2b9725e
Aju <94348613+aju001@users.noreply.github.com>
2021-11-25 07:22:58 +0530

    A-J-U

--- technews.js
+++ technews.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.favicon, 'eror.jpg', `*Title:* ${json.data.title}\n*News:* ${json.data.description}`, m, 0, { thumbnail: await (await fetch('https://telegra.ph/file/99fc5c91c8dfcdc58a557.jpg')).buffer() })
+  conn.sendFile(m.chat, json.data.favicon, 'eror.jpg', `*Title:* ${json.data.title}\n*News:* ${json.data.description}`, m, 0, { thumbnail: await (await fetch('https://i.ibb.co/0Cg9MJN/20210923-185508.jpg')).buffer() })
 }
 handler.help = ['technews']
 handler.tags = ['tools']
