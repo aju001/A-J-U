0ed2555cf6a8661d16780aad3b3125990b3a57e9
Aju <94348613+aju001@users.noreply.github.com>
2021-11-27 00:53:58 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -684,7 +684,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
 
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
 
-    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'made with ❤️ by ariffb', 'Pemilik Bot', `${_p}owner`, 'Donasi', `${_p}donasi`, m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), text.trim(), 'made with Lub by Aju', 'Bot Owner', `${_p}owner`, '🙌', `${_p}❤asi`, m)
 
   } catch (e) {
 
